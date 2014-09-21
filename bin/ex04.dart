62dbac8ee6ac1735a13731d13b9597efdf1e6884
OlivierTremblay9 <oli_0007@hotmail.com>
2014-09-21 17:22:26 -0400

    Final version

--- bin/ex04.dart
+++ bin/ex04.dart
@@ -29,6 +29,12 @@ String fibonacciIteration(int f) {
   return result;
 }
 
+DateTime Date(DateTime i, DateTime j) { 
+ Duration durationDiff = j.difference(i); 
+ DateTime dateDiff = new DateTime(0).add(durationDiff); 
+ return dateDiff; 
+} 
+
 
 
 main() {
@@ -37,86 +43,66 @@ main() {
 
   String sequence = fibonacciIteration(numbers);
   
-  print('Num_01: Fibonacci sequence with the first ''${numbers} numbers and starting with 0');
+  print('Num_01:\n        Fibonacci sequence with the first ''${numbers} numbers and starting with 0');
   
-  print('         -Result: [$sequence]');
+  print('        Result: [$sequence]');
   
   print('Num_02:');
 
   num sin45 = 0.707;
   num cos45 = sin45; 
-  var x = 0;
-  var y = 0;
   num center_X = 400;
   num center_Y = 300;
   int hyp = 160;
-  var width = 800;
-  var height = 600;
-  
+  var x = 0;
+  var y = 0;
   var segment_AB= sin45 * hyp;
-  var segment_AC= cos45 * hyp;  
-  
+  var segment_AC= cos45 * hyp;    
   var widtghpoint= x + center_X + segment_AB;
   var heightpoint= y + center_Y + segment_AC;
   
-  print('        Result: X = ${heightpoint} & Y = ${widtghpoint} pixels');
+  print('        Result: X = ${heightpoint} pixels & Y = ${widtghpoint} pixels');
   
-  print('Num_03:');
+  print('Num_03:'); // I was inspired by the code of the code of Mr. Warren Zachari-Lavoie to complete this exercise. Link: https://github.com/zachwarren1/SIO-2109_Ex04.git
   
-  DateTime mybirthday = DateTime.parse("1983-09-19 24:00:00");
-  DateTime now = new DateTime.now();
-  
-  Duration difference = now.difference(mybirthday);
-
-  
-  
-
-  print('Result: ${difference.inDays} Days');
+     DateTime now = new DateTime.now(); 
+     DateTime nowmidnight = new DateTime(now.year, now.month, now.day, 24); 
+     DateTime birthdaymidnight = new DateTime(1983, 09, 13, 24); 
+     DateTime diff = Date(birthdaymidnight, nowmidnight); 
+  print('        Time in years, months and days passed between your birthday \n        at midnight and today at midnight.\n        Answer: ${diff.year} ans, ${diff.month} mois et ${diff.day} jours'); 
+   
   
   print('Num_04:');
   
-  var value1 =0; 
+     var value1 =0; 
      var value2 = 1000;
      var value3 = 1.048;
     
-   
      for (var n=0 ; n< 5 ;n++){ 
-       if (n==0){ 
-         value2=1000; 
-         
+      if (n==0){ 
+         value2=1000;   
        } 
-       else{ 
-         
-         value2=value1;
-        
-         
+      else{ 
+         value2=value1; 
        } 
-        
        value1=value2*value3; 
-       print(prepare(value1,value2,value3)); 
-  
- 
+       print('        ${prepare(value1,value2,value3)}'); 
        }
      
-     print('Num_05:'); 
-         var v1 =0; 
-         var v2 = 0;
-         var v3 = 8;
-        
-       
-         for (var n=0 ; n< 16 ;n++){ 
-           if (n==0){ 
-             v2=v2; 
-             
-           } 
-           else{ 
-             v2=v2 +1;
-            
-             
-           } 
-            
-           v1=v2*v3; 
-           print(table(v1,v2,v3));
+   print('Num_05:'); 
+      var v1 =0; 
+      var v2 = 0;
+      var v3 = 8;
+ 
+     for (var n=0 ; n< 16 ;n++){ 
+      if (n==0){ 
+          v2=v2;        
+       } 
+      else{ 
+          v2=v2 +1;      
+       }     
+          v1=v2*v3; 
+    print('        ${table(v1,v2,v3)}');
    }
          
 }     
